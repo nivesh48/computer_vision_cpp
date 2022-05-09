027afd9087df41d63d7bdb0bc4c3b2043595df65
nivesh48 <nivesh48@gmail.com>
2022-05-09 17:38:26 +0800

    added first cpp file

---
+++ cpp_starter_tutorials/HelloWorld.cpp
@@ -0,0 +1,10 @@
+// my first program in C++
+#include <iostream>
+using namespace std;
+int main() {
+
+  cout << "Hello World! \n";
+  cout << "Welcome to your first CPP program \n";
+  return 0;
+  
+}
